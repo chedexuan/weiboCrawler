8bda0d3edb178869a3aa26e0e5abdcb0e0372020
chedexuan <842900956@qq.com>
2023-03-04 14:10:23 +0800

    test

---
+++ src/main/java/com/cdx/weibocrawler/WeiboCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.cdx.weibocrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeiboCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeiboCrawlerApplication.class, args);
+    }
+
+}
